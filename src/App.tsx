2ed5e552576c65d07d7ffc1a20ec9c7ee1247bfb
semen <thaisun2018@yandex.ru>
2023-05-18 21:15:45 +0300

    add hook useTheme with ThemeProvider based on createContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import { Suspense } from 'react';
+import { Suspense, useState, useContext } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
-import './index.scss';
+import './styles/index.scss';
 
 import { MainPageSync } from './pages/MainPage/MainPage.async';
 import { AboutPageSync } from './pages/AboutPage/AboutPage.async';
 
+import { useTheme } from './theme/useTheme';
+
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className='app'>
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>CHANGE</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
