3a886b87313d447330e69bd2466e0dc2e01c2db5
semen <thaisun2018@yandex.ru>
2023-07-06 08:27:27 +0300

    add jest and first test

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,45 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+  test('with one param', () => {
+    expect(classNames('class')).toBe('class');
+  });
+
+  test('with additional class', () => {
+    const expected = 'class class1 class2';
+    expect(classNames('class', {}, ['class1', 'class2'])).toBe(expected);
+  });
+
+  test('with mods', () => {
+    const expected = 'class class1 class2 mod1 mod2';
+    expect(
+      classNames(
+        'class',
+        { mod1: true, mod2: true },
+        ['class1', 'class2'],
+      ),
+    ).toBe(expected);
+  });
+
+  test('with mods false', () => {
+    const expected = 'class class1 class2 mod1';
+    expect(
+      classNames(
+        'class',
+        { mod1: true, mod2: false },
+        ['class1', 'class2'],
+      ),
+    ).toBe(expected);
+  });
+
+  test('with mods undefined', () => {
+    const expected = 'class class1 class2 mod1';
+    expect(
+      classNames(
+        'class',
+        { mod1: true, mod2: undefined },
+        ['class1', 'class2'],
+      ),
+    ).toBe(expected);
+  });
+});
