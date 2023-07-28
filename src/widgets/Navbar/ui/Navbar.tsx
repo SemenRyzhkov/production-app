a044e7f7cd872bf5acf1703643e8b989add1bacd
semen <thaisun2018@yandex.ru>
2023-07-28 16:53:14 +0300

    update Sidebar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,6 @@
 import { useTranslation } from 'react-i18next';
 
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import CssClasses from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -14,18 +13,7 @@ export const Navbar = (props: NavbarProps) => {
 
   return (
     <div className={classNames(CssClasses.Navbar, {}, [className])}>
-      <div className={CssClasses.links}>
-        <AppLink
-          theme={AppLinkTheme.RED}
-          to='/'
-          className={CssClasses.mainLink}
-        >
-          {t('Главная')}
-        </AppLink>
-        <AppLink theme={AppLinkTheme.SECONDARY} to='/about'>
-          {t('О сайте')}
-        </AppLink>
-      </div>
+      <div className={CssClasses.links}>/</div>
     </div>
   );
 };
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -4,7 +4,11 @@ import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/ui/LangSwitcher';
 import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
 import { useTranslation } from 'react-i18next';
-import CssClasses from './Sidebar.module.scss';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import AboutIcon from 'shared/assets/icons/about-20-20.svg';
+import MainIcon from 'shared/assets/icons/main-20-20.svg';
+import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
   className?: string;
@@ -13,33 +17,49 @@ interface SidebarProps {
 export const Sidebar = ({ className }: SidebarProps) => {
   const [collapsed, setCollapsed] = useState(false);
   const { t } = useTranslation();
+
   const onToggle = () => {
-    // в хук setState можно передавать колбэк, который принимает предыдущее значение
     setCollapsed((prev) => !prev);
   };
+
   return (
     <div
       data-testid='sidebar'
-      className={classNames(
-        CssClasses.Sidebar,
-        { [CssClasses.collapsed]: collapsed },
-        [className]
-      )}
+      className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [
+        className,
+      ])}
     >
       <Button
         data-testid='sidebar-toggle'
-        type='button'
         onClick={onToggle}
-        className='collapseBtn'
+        className={cls.collapseBtn}
         theme={ButtonTheme.BACKGROUND_INVERTED}
-        square
         size={ButtonSize.L}
+        square
       >
         {collapsed ? '>' : '<'}
       </Button>
-      <div className={CssClasses.swither}>
+      <div className={cls.items}>
+        <AppLink
+          theme={AppLinkTheme.SECONDARY}
+          to={RoutePath.main}
+          className={cls.item}
+        >
+          <MainIcon className={cls.icon} />
+          <span className={cls.link}>{t('Главная')}</span>
+        </AppLink>
+        <AppLink
+          theme={AppLinkTheme.SECONDARY}
+          to={RoutePath.about}
+          className={cls.item}
+        >
+          <AboutIcon className={cls.icon} />
+          <span className={cls.link}>{t('О сайте')}</span>
+        </AppLink>
+      </div>
+      <div className={cls.switchers}>
         <ThemeSwitcher />
-        <LangSwitcher className={CssClasses.lang} />
+        <LangSwitcher short={collapsed} className={cls.lang} />
       </div>
     </div>
   );
