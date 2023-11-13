8c737801cd896f1dee0393443b271429030d16e1
semen <thaisun2018@yandex.ru>
2023-11-13 07:40:21 +0300

    article entity and skeleton shared component

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -1,21 +1,32 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
+import { ArticleDetails } from 'entities/Article';
+import { useParams } from 'react-router-dom';
 import cls from './ArticleDetailsPage.module.scss';
 
 interface ArticleDetailsPageProps {
-  className?: string;
+    className?: string;
 }
 
 const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
-  const { className } = props;
-  const { t } = useTranslation();
+    const { className } = props;
+    const { t } = useTranslation('article-details');
+    const { id } = useParams<{ id: string }>();
 
-  return (
-    <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
-      ARTICLE DETAILS
-    </div>
-  );
+    if (!id) {
+        return (
+            <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+                {t('Статья не найдена')}
+            </div>
+        );
+    }
+
+    return (
+        <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+            <ArticleDetails id={id} />
+        </div>
+    );
 };
 
 export default memo(ArticleDetailsPage);
