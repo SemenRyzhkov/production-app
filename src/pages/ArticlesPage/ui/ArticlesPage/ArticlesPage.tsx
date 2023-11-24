a8ba03b5c6e90c2e4a2c6f7b7620b79b7e35bf1a
semen <thaisun2018@yandex.ru>
2023-11-24 08:07:39 +0300

    create artcle list and article list item entiries

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -1,6 +1,7 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
+import { ArticleList, ArticleView } from 'entities/Article';
 import cls from './ArticlesPage.module.scss';
 
 interface ArticlesPageProps {
@@ -11,7 +12,11 @@ const ArticlesPage = (props: ArticlesPageProps) => {
   const { className } = props;
   const { t } = useTranslation();
 
-  return <div className={classNames(cls.ArticlesPage, {}, [className])} />;
+  return (
+    <div className={classNames(cls.ArticlesPage, {}, [className])}>
+      <ArticleList isLoading view={ArticleView.BIG} articles={[]} />
+    </div>
+  );
 };
 
 export default memo(ArticlesPage);
