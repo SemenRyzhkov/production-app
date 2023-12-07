3c14ecae8555b9ffe2a4d60914db3ad45792f5df
semen <thaisun2018@yandex.ru>
2023-12-07 11:51:45 +0300

    fix

--- src/widgets/Page/Page.tsx
+++ src/widgets/Page/Page.tsx
@@ -6,9 +6,10 @@ import { getUIScrollByPath, uiActions } from 'features/UI';
 import { useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { StateSchema } from 'app/providers/StoreProvider';
-import cls from './Page.module.scss';
 import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
 import { useThrottle } from 'shared/lib/hooks/useThrottle/useThrottle';
+
+import cls from './Page.module.scss';
 
 interface PageProps {
   className?: string;
